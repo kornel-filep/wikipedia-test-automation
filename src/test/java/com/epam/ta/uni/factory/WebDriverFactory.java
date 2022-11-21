c2d1d0c9767c2167a2b63ad4b8bb6773743d14ec
Kornel Filep <kornel.filep@gmail.com>
2022-11-21 13:47:47 +0100

    adding browserstack support

--- src/test/java/com/epam/ta/uni/factory/WebDriverFactory.java
+++ src/test/java/com/epam/ta/uni/factory/WebDriverFactory.java
@@ -1,16 +1,30 @@
 package com.epam.ta.uni.factory;
 
+import com.epam.ta.uni.browserstack.Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -21,6 +35,10 @@ public class WebDriverFactory {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebDriverFactory.class);
     private static final String CHROME = "chrome";
     private static final String FIREFOX = "firefox";
+
+    private static final String REMOTE = "browserstack";
+
+    private final JSONObject TEST_CONFIG = this.parseWebDriverConfig();
 
     @Value("${browserName:chrome}")
     private String browserName;
@@ -39,6 +57,9 @@ public class WebDriverFactory {
                 case FIREFOX:
                     webDriver = setUpFirefoxDriver();
                     break;
+                case REMOTE:
+                    webDriver = createWebDriverForPlatform(this.getPlatforms().get(0));
+                    break;
                 default:
                     throw new RuntimeException("Unsupported driver for browser=" + browserName);
             }
@@ -49,6 +70,37 @@ public class WebDriverFactory {
         webDriver.manage().timeouts().implicitlyWait(PAGE_OR_ELEMENT_LOAD_WAIT_SECONDS, TimeUnit.SECONDS);
 
         return webDriver;
+    }
+
+    private JSONObject parseWebDriverConfig() {
+        JSONParser parser = new JSONParser();
+        String capabilitiesConfigFile = System.getProperty("caps", "src/test/resources/conf/single.conf.json");
+        try {
+            JSONObject testConfig = (JSONObject) parser.parse(new FileReader(capabilitiesConfigFile));
+            return testConfig;
+        } catch (IOException | ParseException var6) {
+            throw new Error("Unable to parse capabilities file " + capabilitiesConfigFile, var6);
+        }
+    }
+
+    public List<JSONObject> getPlatforms() {
+        JSONArray environments = (JSONArray) TEST_CONFIG.get("environments");
+        List<JSONObject> platforms = new ArrayList<>();
+        for (Object obj : environments) {
+            JSONObject singleConfig = Utility.getCombinedCapability((Map<String, String>) obj, TEST_CONFIG);
+            platforms.add(singleConfig);
+        }
+        return platforms;
+    }
+
+    public WebDriver createWebDriverForPlatform(JSONObject platform) {
+        try {
+            String url = String.format("https://%s/wd/hub", TEST_CONFIG.get("server"));
+            MutableCapabilities caps = new MutableCapabilities(platform);
+            return new RemoteWebDriver(new URL(url), caps);
+        } catch (MalformedURLException var4) {
+            throw new Error("Unable to create WebDriver", var4);
+        }
     }
 
     private WebDriver setUpChromeDriver() {
